7f5ccff5e518b8a1425171da32a6f43d29cde135
Colm Doyle <collydoyle_88@hotmail.com>
2020-08-25 17:11:36 +0000

    added competitions, models and comps.json

---
+++ comps/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+
+class Comp(models.Model):
+    name = models.CharField(max_length=254)
+    size_range = models.TextField()
+    description = models.TextField()
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    entries = models.DecimalField(max_digits=4, decimal_places=0)
+    image_url = models.URLField(max_length=1024)
+    image = models.ImageField(null=True, blank=True)
+    end_date = models.DateField()
+
+    def __str__(self):
+        return self.name
