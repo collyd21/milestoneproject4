78253e5d8c6aa970e5e37d4cba5b627a7b5654fc
Colm Doyle <collydoyle_88@hotmail.com>
2020-08-25 17:47:38 +0000

    added basic competition templates and views

--- comps/views.py
+++ comps/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from .models import Comp
 
 # Create your views here.
+
+def all_comps(request):
+    """ A view to show all competitions """
+
+    comps = Comp.objects.all()
+
+    context = {
+        'comps': comps,
+    }
+
+    return render(request, 'comps/comps.html', context)
