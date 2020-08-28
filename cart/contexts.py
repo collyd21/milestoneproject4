91c466b786e9fa818c5fc4f291831073db51046c
Colm Doyle <collydoyle_88@hotmail.com>
2020-08-28 15:26:02 +0000

    update cart functionality, template and CSS

---
+++ cart/contexts.py
@@ -0,0 +1,30 @@
+from decimal import Decimal
+from django.conf import settings
+from django.shortcuts import get_object_or_404
+from comps.models import Comp
+
+
+def cart_contents(request):
+
+    cart_items = []
+    total = 0
+    comp_count = 0
+    cart = request.session.get('cart', {})
+
+    for item_id, quantity in cart.items():
+        comp = get_object_or_404(Comp, pk=item_id)
+        total += quantity * comp.price
+        comp_count += quantity
+        cart_items.append({
+            'item_id': item_id,
+            'quantity': quantity,
+            'comp': comp,
+        })
+
+    context = {
+        'cart_items': cart_items,
+        'total': total,
+        'comp_count': comp_count,
+    }
+
+    return context
--- cart/views.py
+++ cart/views.py
@@ -12,7 +12,7 @@ def add_to_cart(request, item_id):
 
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
-    cart = request.session.get('bag', {})
+    cart = request.session.get('cart', {})
 
     if item_id in list(cart.keys()):
         cart[item_id] += quantity
@@ -20,5 +20,4 @@ def add_to_cart(request, item_id):
         cart[item_id] = quantity
 
     request.session['cart'] = cart
-    print(request.session['cart'])
     return redirect(redirect_url)
