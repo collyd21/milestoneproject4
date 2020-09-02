418ac20a7df60e393e329f8fa61b2b92f0a14250
Colm Doyle <collydoyle_88@hotmail.com>
2020-09-02 17:43:15 +0000

    created Profiles app, add basic template, URLs and views

--- ms4/urls.py
+++ ms4/urls.py
@@ -26,4 +26,5 @@ urlpatterns = [
     path('cart/', include('cart.urls')),
     path('checkout/', include('checkout.urls')),
     path('contact/', include('contact.urls')),
+    path('profiles/', include('profiles.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
