fb74f843880faa013508359e642fb2aba2c45344
Colm Doyle <collydoyle_88@hotmail.com>
2020-09-04 17:51:08 +0000

    add update user details function to profile

--- profiles/models.py
+++ profiles/models.py
@@ -4,6 +4,7 @@ from django.db.models.signals import post_save
 from django.dispatch import receiver
 
 
+
 class UserProfile(models.Model):
     """
     A user profile model for maintaining default
@@ -11,12 +12,13 @@ class UserProfile(models.Model):
     """
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     default_phone_number = models.CharField(max_length=20, null=True, blank=True)
-    default_country = models.CharField(max_length=40, null=False, blank=False)
-    default_postcode = models.CharField(max_length=20, null=True, blank=True)
-    default_town_city = models.CharField(max_length=40, null=True, blank=True)
     default_address = models.CharField(max_length=80, null=True, blank=True)
     default_address2 = models.CharField(max_length=80, null=True, blank=True)
+    default_town_city = models.CharField(max_length=40, null=True, blank=True)
     default_county = models.CharField(max_length=80, null=True, blank=True)
+    default_postcode = models.CharField(max_length=20, null=True, blank=True)
+    default_country = models.CharField(max_length=50, null=True, blank=True)
+    
 
     def __str__(self):
         return self.user.username
