62f15f95c09688d29ddc9820a5ca0e8e2a09ee0a
Colm Doyle <collydoyle_88@hotmail.com>
2020-09-05 14:29:34 +0000

    updated function for adding new comps

--- comps/models.py
+++ comps/models.py
@@ -7,11 +7,11 @@ class Comp(models.Model):
         ordering = ['-date']
 
     name = models.CharField(max_length=254)
-    size_range = models.TextField()
+    size_range = models.CharField(max_length=254)
     description = models.TextField()
     price = models.DecimalField(max_digits=6, decimal_places=2)
     entries = models.DecimalField(max_digits=4, decimal_places=0)
-    image_url = models.URLField(max_length=1024)
+    image_url = models.URLField(max_length=1024, null=True, blank=True)
     image = models.ImageField(null=True, blank=True)
     date = models.DateTimeField(auto_now_add=True)
 
